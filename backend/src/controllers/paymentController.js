3101996f41a3a49ed13bfc885c306e7658a5835d
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-08 15:30:28 +0100

    Add paymentController with createPayment and updatePayment functions

---
+++ backend/src/controllers/paymentController.js
@@ -0,0 +1,55 @@
+import Payment from "../models/Payment";
+
+export const paymentController = {
+
+  createPayment: async (req, res) => {
+    
+    try {
+      const { amount } = req.body;
+  
+      if (!amount) {
+        return res.status(400).json({ error: 'Please provide all required fields' });
+      }
+  
+      const newPayment = new Payment({ amount});
+      const savedPayment = await newPayment.save();
+  
+      res.status(201).json({
+        message: 'Payment created successfully',
+        Payment: savedPayment,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+
+  updatePayment: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { amount } = req.body;
+  
+      if (!amount) {
+        return res.status(400).json({ error: 'Please provide all required fields' });
+      }
+  
+      const updatedPayment = await Payment.findByIdAndUpdate(
+        id,
+        { amount },
+        { new: true }
+      );
+  
+      res.status(200).json({
+        message: 'Payment updated successfully',
+        Payment: updatedPayment,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+  
+
+  
+
+};      
