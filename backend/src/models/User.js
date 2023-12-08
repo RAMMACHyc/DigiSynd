7c7eae6f552848a176037fcb285b6f26a428b1af
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-08 15:31:33 +0100

    Add User model to backend

---
+++ backend/src/models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'Please add a firstName'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Please add a lastName'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+
+    phone: {
+      type: Number,
+      required: true,
+    },
+
+    payment: {
+        type: Number,
+        required: true
+    },
+
+    role: {
+      type: String,
+      enum: ['syndic', 'superAdmin'],
+      default: 'syndic',
+    },
+     
+
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('User', userSchema)
