c0ad8455e2008b5204dc301b6fa6e99815b0d88b
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-13 21:52:02 +0100

    Fix resident field indentation

--- backend/src/models/Apartment.js
+++ backend/src/models/Apartment.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const ApartmentSchema = new mongoose.Schema({
     number: { type: Number, required: [true, "please"] },
     etage: { type: Number, required: [true, "please"] },
-    resident: { type: String, required: [true, "please"] },
-
+    resident: { type: String, required: [true, "please"] },    
     createdAt: {
         type: Date,
         default: new Date(),
