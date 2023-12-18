01a291efd2c0c8ba110d47f58be357e09f368cc3
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-18 18:03:52 +0100

    Refactor paymentController to include year in createPayment and add getPaymentStatus function

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -1,17 +1,17 @@
 import Payment from "../models/Payment";
+import Apartment from "../models/Apartment";
 
 export const paymentController = {
-
   createPayment: async (req, res) => {
-    
     try {
-        const { month, apartmentId } = req.body;
-      if (!month) {
+      const { apartmentId, month, year } = req.body;
+      if (!apartmentId || !month || !year) {
         return res.status(400).json({ error: 'Please provide all required fields' });
       }
-      const newPayment = new Payment({ month, apartment: apartmentId, ownerId: req.user._id});
+      const newPayment = new Payment({ apartment: apartmentId, month, year });
+
       const savedPayment = await newPayment.save();
-  
+
       res.status(201).json({
         message: 'Payment created successfully',
         Payment: savedPayment,
@@ -20,23 +20,23 @@ export const paymentController = {
       console.error(error);
       res.status(500).json({ error: 'Internal server error' });
     }
-  }, 
+  },
 
   updatePayment: async (req, res) => {
     try {
       const { id } = req.params;
       const { amount } = req.body;
-  
+
       if (!amount) {
         return res.status(400).json({ error: 'Please provide all required fields' });
       }
-  
+
       const updatedPayment = await Payment.findByIdAndUpdate(
         id,
         { amount },
         { new: true }
       );
-  
+
       res.status(200).json({
         message: 'Payment updated successfully',
         Payment: updatedPayment,
@@ -46,17 +46,28 @@ export const paymentController = {
       res.status(500).json({ error: 'Internal server error' });
     }
   },
-  getPayments: async (req, res) => {
+
+  getPaymentStatus: async (req, res) => {
     try {
-     const Payments = await Payment.find().populate('apartment');
-      res.status(200).json({ Payments });
+      const { year, month } = req.query;
+      const apartments = await Apartment.find().exec();
+  
+      const StatusArray = [];
+  
+      for (const apartment of apartments) {
+        const payments = await Payment.find({ apartment: apartment._id, year, month }).exec();
+        const paymentStatus = payments.length > 0;
+        StatusArray.push({ apartmentId: apartment._id, paymentStatus });
+      }
+  
+      res.status(200).json({ StatusArray });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({
+        error: 'Internal server error',
+      });
     }
-  },
+  }
   
-
+};
   
-
-};      
