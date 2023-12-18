463ce46793f8fa1a3424e049d77b7d7a088c3502
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-18 18:04:04 +0100

    Refactor authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,34 +1,30 @@
-import { verify } from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
-import { findById } from '../models/User'
+import { verify } from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/User';  
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token
+  let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
+      token = req.headers.authorization.split(' ')[1];
     
-      token = req.headers.authorization.split(' ')[1]
+      const decoded = verify(token, process.env.JWT_SECRET);
 
-      const decoded = verify(token, process.env.JWT_SECRET)
+      req.user = await User.findById(decoded.id).select('-password');
 
-      req.user = await findById(decoded.id).select('-password')
-
-      next()
+      next();
     } catch (error) {
-      console.log(error)
-      res.status(401)
-      throw new Error('Not authorized')
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized');
     }
   }
 
   if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
+    res.status(401);
+    throw new Error('Not authorized, no token');
   }
-})
+});
 
-export default { protect }
+export default protect;
