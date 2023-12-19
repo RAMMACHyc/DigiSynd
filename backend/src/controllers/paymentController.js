ef7085f5f890ca52661b9a8d3673436d2e2d2919
RAMMACHyc <sadik.ram12@gmail.com>
2023-12-19 23:28:22 +0100

    Add deletePayment function to paymentController

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -78,6 +78,18 @@ export const paymentController = {
       res.status(500).json({ error: 'Internal server error' });
     }
   },
+
+  deletePayment: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Payment.findByIdAndDelete(id);
+      res.status(200).json({ message: 'Payment deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+  
   
 };
   
